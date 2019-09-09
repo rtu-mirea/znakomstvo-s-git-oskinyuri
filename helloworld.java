16b56accb97f611566010676eae093e35597a168
Oskin Yuri <Oskin.Y.A@sberbank.ru>
2019-09-09 17:36:06 +0300

    third commit

--- helloworld.java
+++ helloworld.java
@@ -7,5 +7,6 @@ public class Main
         System.out.println("Hello world");
         System.out.println("lol kek");
         System.out.println("4eburek");
+        System.out.println("4eburek");
     }
 }
