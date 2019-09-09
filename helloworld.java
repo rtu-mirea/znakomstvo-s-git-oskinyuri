17591961edf86fa0a6148ba7a7d7895f2a1c61b0
oskinyuri <oskinyuri@gmail.com>
2019-09-09 17:41:30 +0300

    Develop (#1)
    Hellow world for Lab 1

--- helloworld.java
+++ helloworld.java
@@ -5,5 +5,8 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Hello world");
+        System.out.println("lol kek");
+        System.out.println("4eburek");
+        System.out.println("4eburek");
     }
 }
